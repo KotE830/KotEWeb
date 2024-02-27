411dab3a4e26a1bd12ad6d47c038f8745eac1f1a
KotE <ham10233@naver.com>
2024-02-27 17:17:22 +0900

    Add errors

---
+++ milddlewares/error-handler.js
@@ -0,0 +1,11 @@
+function handleErrors(error, req, res, next) {
+  console.log(error);
+
+  if (error.code === 404) {
+    return res.status(404).render("shared/errors/404");
+  }
+
+  res.status(500).render("shared/errors/500");
+}
+
+module.exports = handleErrors
